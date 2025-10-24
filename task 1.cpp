1c506922225aca3194b26e44b27c8839461954e8
Arwa Adnan <f2025-0159@bnu.edu.pk>
2025-10-24 14:48:04 +0500

    Create task 1.cpp

---
+++ task 1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+
+  cout<<"Name:\tArwa Adnan\n";
+    cout<<"Department:\tSCIT\n";
+    cout<<"Qoute:\t\"Learning never exhaust the mind .\"\n";
+    return 0;
+}
